050eedafe69561fac0e80b3863b38cd7b9a6e59a
Anna-Anka <anjbekrenewa@gmail.com>
2025-02-21 12:50:46 +0300

    first commit

---
+++ src/js/#tippy/#swipers/#mobile.js
@@ -0,0 +1,38 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+Swiper.use([Navigation]);
+
+if (document.querySelector('.__swiper')) {
+    const slider = document.querySelector('.__swiper');
+    let mySwiper;
+
+    function mobileSlider() {
+        if (window.innerWidth <= 560 && slider.dataset.mobile === 'false') {
+            mySwiper = new Swiper(slider, {
+                slidesPerView: 1,
+                spaceBetween: 10,
+                loop: true,
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+            });
+
+            slider.dataset.mobile = 'true';
+        }
+
+        if (window.innerWidth > 560) {
+            slider.dataset.mobile = 'false';
+            if (slider.classList.contains('swiper-container-initialized')) {
+                mySwiper.destroy();
+            }
+        }
+    }
+
+    mobileSlider();
+
+    window.addEventListener('resize', () => {
+        mobileSlider();
+    });
+}
