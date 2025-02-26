b115c34a20012f043320b234c32ad5127730afd1
Anna-Anka <anjbekrenewa@gmail.com>
2025-02-26 17:13:13 +0300

    add hero template and add home-hero block

---
+++ src/js/project/slider/_home-hero-slider.js
@@ -0,0 +1,25 @@
+import Swiper from 'swiper';
+import { A11y, Navigation, Autoplay } from 'swiper/modules';
+
+Swiper.use([A11y, Navigation, Autoplay]);
+
+if (document.querySelector('.home-hero__swiper')) {
+    new Swiper('.home-hero__swiper', {
+        loop: true,
+        speed: 800,
+        slidesPerGroup: 1,
+        slidesPerView: 1,
+        centeredSlides: true,
+
+        autoplay: {
+            delay: 10000,
+            disableOnInteraction: false,
+            stopOnlastSlide: false,
+        },
+
+        navigation: {
+            nextEl: '.home-hero__button--next',
+            prevEl: '.home-hero__button--prev',
+        },
+    });
+}
--- src/js/templates/index.js
+++ src/js/templates/index.js
@@ -1,1 +1,3 @@
 import './_burger.js';
+import './_get-header-height.js';
+import './_set-dvh.js';
