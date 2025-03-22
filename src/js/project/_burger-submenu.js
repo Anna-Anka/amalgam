548f6911415a35bb6f8241ef147f81f454407f71
Anna-Anka <anjbekrenewa@gmail.com>
2025-03-22 20:41:35 +0300

    add fix submenu

--- src/js/project/_burger-submenu.js
+++ src/js/project/_burger-submenu.js
@@ -1,14 +1,52 @@
-const changeState = (submenu, button) => {
-    if (submenu.classList.contains('burger-menu__submenu--active')) {
-        button.setAttribute('aria-expanded', false)
-        button.setAttribute('aria-label', 'Открыть дополнительное меню')
-        submenu.classList.remove('burger-menu__submenu--active')
-        button.classList.remove('burger-menu__button--open')
+import { globalVars } from '../utils/index.js';
+
+const allMainSubmenu = []
+const allSubmenu = {}
+
+const focusCatch = (e) => {
+    allMainSubmenu.forEach((menu) => {
+        const nodes = menu.querySelectorAll(globalVars.focusEl);
+        const nodesArray = Array.prototype.slice.call(nodes);
+        const focusedItemIndex = nodesArray.indexOf(document.activeElement)
+        if (e.shiftKey && focusedItemIndex === 0) {
+            nodesArray[nodesArray.length - 1].focus();
+            e.preventDefault();
+        }
+        if (!e.shiftKey && focusedItemIndex === nodesArray.length - 1) {
+            nodesArray[0].focus();
+            e.preventDefault();
+        }
+    })
+}
+
+const closeMenu = (menu, button) => {
+    button.setAttribute('aria-expanded', false)
+    button.setAttribute('aria-label', 'Открыть дополнительное меню')
+    menu.classList.remove('burger-menu__submenu--active')
+    button.classList.remove('burger-menu__button--open')
+}
+
+const openMenu = (menu, button) => {
+    button.setAttribute('aria-expanded', true)
+    button.setAttribute('aria-label', 'Закрыть дополнительное меню')
+    menu.classList.add('burger-menu__submenu--active')
+    button.classList.add('burger-menu__button--open')
+}
+
+const closeAllSubmenu = () => {
+    for (let key in allSubmenu) {
+        const menu = allSubmenu[key].menu
+        const button = allSubmenu[key].button
+
+        closeMenu(menu, button)
+    }
+}
+
+const changeState = (menu, button) => {
+    if (menu.classList.contains('burger-menu__submenu--active')) {
+        closeMenu(menu, button)
     } else {
-        button.setAttribute('aria-expanded', true)
-        button.setAttribute('aria-label', 'Закрыть дополнительное меню')
-        submenu.classList.add('burger-menu__submenu--active')
-        button.classList.add('burger-menu__button--open')
+        openMenu(menu, button)
     }
 }
 
@@ -20,6 +58,7 @@ const changeState = (submenu, button) => {
     if (!mainButtons) {
         return
     }
+
     mainButtons.forEach((mainButton, index) => {
         const menu = mainButton.nextElementSibling
         const id = `header-burger-submenu-main-${index}`
@@ -27,13 +66,33 @@ const changeState = (submenu, button) => {
         mainButton.setAttribute('aria-controls', id)
         menu.setAttribute('id', id)
 
+        allMainSubmenu.push(menu)
+
         mainButton.addEventListener('click', () => {
             changeState(menu, mainButton)
         })
 
         document.addEventListener('click', (event) => {
-            if (!mainButton.contains(event.target) && !menu.contains(event.target)) {
-                changeState(menu, mainButton)
+            const target = event.target
+
+            if (menu.classList.contains('burger-menu__submenu--active')) {
+                if (!mainButton.contains(target) && !menu.contains(target)) {
+                    closeMenu(menu, mainButton)
+                    closeAllSubmenu()
+                }
+            }
+        })
+
+        document.addEventListener('keydown', (event) => {
+            if (menu.classList.contains('burger-menu__submenu--active')) {
+                if (event.key === 'Escape') {
+                    closeMenu(menu, mainButton)
+                    closeAllSubmenu()
+                }
+
+                if (event.which) {
+                    focusCatch(event);
+                }
             }
         })
     })
@@ -51,6 +110,13 @@ const changeState = (submenu, button) => {
 
         submenu.setAttribute('id', id)
 
+        allSubmenu[index] = {
+            parent: container,
+            button: button,
+            arrow: arrow,
+            menu: submenu
+        }
+
         if (isTouchDevice) {
             // Если телефон
             button.removeAttribute('aria-controls')
@@ -65,6 +131,7 @@ const changeState = (submenu, button) => {
             arrow.addEventListener('click', () => {
                 changeState(submenu, arrow)
             })
+
         } else {
             arrow.removeAttribute('aria-controls')
             arrow.removeAttribute('aria-label')
@@ -76,11 +143,23 @@ const changeState = (submenu, button) => {
             button.setAttribute('aria-expanded', false)
 
             container.addEventListener('mouseover', () => {
-                changeState(submenu, button)
+                openMenu(submenu, button)
+            })
+
+            button.addEventListener('keydown', (event) => {
+                const target = event.target
+                const targetMenu = target.closest('[data-burger-submenu-container]').querySelector('.burger-menu__submenu')
+
+                if (event.key === 'Enter') {
+                    closeAllSubmenu()
+                    if (button.contains(target)) {
+                        openMenu(targetMenu, target)
+                    }
+                }
             })
 
             container.addEventListener('mouseout', () => {
-                changeState(submenu, button)
+                closeMenu(submenu, button)
             })
         }
     })
